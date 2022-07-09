852dfad7c30d8be76ce22eee99489fe83eb5da80
Siddhant <santoshshrivastava39@gmail.com>
2022-07-09 16:45:02 +0530

    Validating password for atleast 1 UpperCase, 1 Numeric Number & 1 Special Character

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -6,7 +6,7 @@ namespace UserRegistrationProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Password should have minimum 8 characters with atleast 1 UpperCase and 1 numeric number -");
+            Console.WriteLine("Password should have minimum 8 characters with atleast 1 UpperCase,1 Numeric Number & 1 SpecialCharacter -");
             UserRegistration.CheckUserPassword();
         }
     }
--- UserRegistrationProblem/UserRegistration.cs
+++ UserRegistrationProblem/UserRegistration.cs
@@ -9,12 +9,12 @@ namespace UserRegistrationProblem
 {
     public class UserRegistration
     {
-        // Checking the Password that should have min 8 characters with atleast 1 UpperCase and 1 Numeric Number
+        // Checking the Password that should have min 8 characters with atleast 1 UpperCase,1 Numeric Number & 1 SpecialCharacter
         public static void CheckUserPassword()
         {
             Console.WriteLine("Enter your Password- ");
             string Password = Console.ReadLine();
-            string PasswordPattern = @"^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$";
+            string PasswordPattern = @"^(?=.*[A-Z])(?=.*[@#$!%^&-+=()])(?=.*[0-9])(?=.*[a-z]).{8,}$";
             Regex PasswordPatternRegex = new Regex(PasswordPattern);
            
             var Result = PasswordPatternRegex.IsMatch(Password);
